3595d14510f069772324cae7e15a04311c0c03da
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2021-01-31 05:31:13 +0600

    implement inter list drag and drop

--- src/App.js
+++ src/App.js
@@ -2,79 +2,125 @@ import React, { useState } from "react";
 import "./App.css";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-const finalSpaceCharacters = [
+
+const mainData = [
   {
-    id: "gary",
-    name: "Gary Goodspeed",
+    listName: "characters1",
+    listArray: [
+      {
+        id: "gary",
+        name: "Gary Goodspeed",
+      },
+      {
+        id: "cato",
+        name: "Little Cato",
+      },
+      {
+        id: "kvn",
+        name: "KVN",
+      },
+      {
+        id: "mooncake",
+        name: "Mooncake",
+      },
+      {
+        id: "quinn",
+        name: "Quinn Ergon",
+      },
+    ],
   },
   {
-    id: "cato",
-    name: "Little Cato",
-  },
-  {
-    id: "kvn",
-    name: "KVN",
-  },
-  {
-    id: "mooncake",
-    name: "Mooncake",
-  },
-  {
-    id: "quinn",
-    name: "Quinn Ergon",
+    listName: "characters2",
+    listArray: [
+      {
+        id: "gary2",
+        name: "Gary Goodspeed 2",
+      },
+      {
+        id: "cato2",
+        name: "Little Cato 2",
+      },
+      {
+        id: "kvn3",
+        name: "KVN 2",
+      },
+      {
+        id: "mooncake4",
+        name: "Mooncake 2",
+      },
+      {
+        id: "quinn5",
+        name: "Quinn Ergon 2",
+      },
+    ],
   },
 ];
-
-const finalSpaceCharacters2 = [
-  {
-    id: "gary2",
-    name: "Gary Goodspeed",
-  },
-  {
-    id: "cato2",
-    name: "Little Cato",
-  },
-  {
-    id: "kvn3",
-    name: "KVN",
-  },
-  {
-    id: "mooncake4",
-    name: "Mooncake",
-  },
-  {
-    id: "quinn5",
-    name: "Quinn Ergon",
-  },
-];
-
-const data = [finalSpaceCharacters,finalSpaceCharacters2];
 
 function App() {
-  const [characters, updateCharacters] = useState(finalSpaceCharacters);
-  const [characters2, updateCharacters2] = useState(finalSpaceCharacters2);
+  const [data, setData] = useState(mainData);
 
   function handleOnDragEnd(result) {
+    const tempData = [...data];
     console.log(result);
 
     if (!result.destination) return;
-    console.log(characters);
-    const items = Array.from(characters);
-    const [reorderedItem] = items.splice(result.source.index, 1);
-    items.splice(result.destination.index, 0, reorderedItem);
+    if (result.destination.droppableId === result.source.droppableId) {
+      const tempArray = tempData.find(
+        (item) => item.listName === result.destination.droppableId
+      );
+      const dataIndex = tempData.findIndex(
+        (item) => item.listName === result.destination.droppableId
+      );
+      const items = JSON.parse(JSON.stringify(tempArray.listArray));
+      const [reorderedItem] = items.splice(result.source.index, 1);
+      items.splice(result.destination.index, 0, reorderedItem);
+      const updatedListItem = {
+        listName: tempArray.listName,
+        listArray: [...items],
+      };
+      tempData[dataIndex] = { ...updatedListItem };
+      setData(tempData);
+    } else {
+      // get the arrays
+      const tempSourceArray = tempData.find(
+        (item) => item.listName === result.source.droppableId
+      );
+      const tempDestinationArray = tempData.find(
+        (item) => item.listName === result.destination.droppableId
+      );
 
-    updateCharacters(items);
-    console.log(items);
+      // get the arrays indexes
+      const dataSourceIndex = tempData.findIndex(
+        (item) => item.listName === result.source.droppableId
+      );
+      const dataDestinationIndex = tempData.findIndex(
+        (item) => item.listName === result.destination.droppableId
+      );
 
+      // update the source list
+      const sourceItems = JSON.parse(JSON.stringify(tempSourceArray.listArray));
+      const movedItem = { ...sourceItems[result.source.index] };
+      sourceItems.splice(result.source.index, 1);
+      const updatedSourceListItem = {
+        listName: tempSourceArray.listName,
+        listArray: [...sourceItems],
+      };
+      tempData[dataSourceIndex] = { ...updatedSourceListItem };
 
-    // if (!result.destination) return;
-    // console.log(characters2);
-    // const items2 = Array.from(characters2);
-    // const [reorderedItem2] = items.splice(result.source.index, 1);
-    // items.splice(result.destination.index, 0, reorderedItem2);
+      // update the destination list
+      const destinationItems = JSON.parse(
+        JSON.stringify(tempDestinationArray.listArray)
+      );
+      destinationItems.splice(result.destination.index, 0, movedItem);
+      const updatedDestinationListItem = {
+        listName: tempDestinationArray.listName,
+        listArray: [...destinationItems],
+      };
+      tempData[dataDestinationIndex] = { ...updatedDestinationListItem };
 
-    // updateCharacters2(items2);
-    // console.log(items2);
+      // set the state with updated value
+      setData(tempData);
+    }
   }
 
   return (
@@ -82,67 +128,38 @@ function App() {
       <header className="App-header">
         <h1>Final Space Characters</h1>
         <DragDropContext onDragEnd={handleOnDragEnd}>
-          <Droppable droppableId="characters">
-            {(provided) => (
-              <ul
-                className="characters"
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {characters.map(({ id, name, thumb }, index) => {
-                  return (
-                    <Draggable key={id} draggableId={id} index={index}>
-                      {(provided) => (
-                        <li
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                        >
-                          <p>{name}</p>
-                        </li>
-                      )}
-                    </Draggable>
-                  );
-                })}
-                {provided.placeholder}
-              </ul>
-            )}
-          </Droppable>
-          
-          <Droppable droppableId="characters2">
-            {(provided) => (
-              <ul
-                className="characters"
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                {characters2.map(({ id, name, thumb }, index) => {
-                  return (
-                    <Draggable key={id} draggableId={id} index={index}>
-                      {(provided) => (
-                        <li
-                          ref={provided.innerRef}
-                          {...provided.draggableProps}
-                          {...provided.dragHandleProps}
-                        >
-                          <p>{name}</p>
-                        </li>
-                      )}
-                    </Draggable>
-                  );
-                })}
-                {provided.placeholder}
-              </ul>
-            )}
-          </Droppable>
+          {data.map((list) => {
+            return (
+              <Droppable droppableId={list.listName} key={list.listName}>
+                {(provided) => (
+                  <ul
+                    className="characters"
+                    {...provided.droppableProps}
+                    ref={provided.innerRef}
+                  >
+                    {list.listArray.map(({ id, name, thumb }, index) => {
+                      return (
+                        <Draggable key={id} draggableId={id} index={index}>
+                          {(provided) => (
+                            <li
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                            >
+                              <p>{name}</p>
+                            </li>
+                          )}
+                        </Draggable>
+                      );
+                    })}
+                    {provided.placeholder}
+                  </ul>
+                )}
+              </Droppable>
+            );
+          })}
         </DragDropContext>
       </header>
-      <p>
-        Images from{" "}
-        <a href="https://final-space.fandom.com/wiki/Final_Space_Wiki">
-          Final Space Wiki
-        </a>
-      </p>
     </div>
   );
 }
