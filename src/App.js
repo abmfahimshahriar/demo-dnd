51656e76bf22c7e0ad820bfaab86aeebf7f44770
abmfahimshahriar <abmfahimshahriar@iut-dhaka.edu>
2021-01-31 03:17:36 +0600

    setup the list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
 
+const finalSpaceCharacters = [
+  {
+    id: 'gary',
+    name: 'Gary Goodspeed',
+  },
+  {
+    id: 'cato',
+    name: 'Little Cato',
+  },
+  {
+    id: 'kvn',
+    name: 'KVN',
+  },
+  {
+    id: 'mooncake',
+    name: 'Mooncake',
+  },
+  {
+    id: 'quinn',
+    name: 'Quinn Ergon',
+  }
+];
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Final Space Characters</h1>
+        <ul className="characters">
+          {finalSpaceCharacters.map(({id, name, thumb}) => {
+            return (
+              <li key={id}>
+                <p>
+                  { name }
+                </p>
+              </li>
+            );
+          })}
+        </ul>
       </header>
+      <p>
+        Images from <a href="https://final-space.fandom.com/wiki/Final_Space_Wiki">Final Space Wiki</a>
+      </p>
     </div>
   );
 }
